c95dbbba37e4912447dca783da5305d88afa2335
parnell <leeparnell@gmail.com>
2021-10-17 15:27:17 -0500

    print_history fixes. other minor fixes

--- bin/print_history.py
+++ bin/print_history.py
@@ -1,61 +1,153 @@
+#!/usr/bin/env python3
 import os
 import sys
+import argparse
+from itertools import chain
 from datetime import datetime
 from collections import defaultdict
 import glob
+from datetime import datetime
+from enum import Enum
 
-def getLine(line, prev):
-    if line.strip().startswith(': '): # zsh history
-        s = line.strip().split(':')
-        etime = datetime.fromtimestamp(int(s[1].strip()))
-        rtime, cmd = s[2].split(';',1)
-        return cmd+'\n', etime, rtime, False
-    elif prev.rstrip().endswith('\\'):
-        return line, None, None, True
-    else:
-        raise Exception("Bash not currently supported")
+class HistoryType(Enum):
+    unknown=0
+    bash=1
+    zsh=2
 
-def print_history(pname, iterm_profile):
-    if pname:
-        D='{}/.history/project/{}'.format(home, pname)
+
+class bcolors:
+    HEADER = "\033[95m"
+    BLUE = "\033[94m"
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    ORANGE = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+class Command():
+    def __init__(self):
+        self.etime = None ## Start time
+        self.rtime = None ## Run time
+        self.cmd = None ## Command
+
+def print_history(
+    pname=None,
+    iterm_profile=None,
+    all_history=None,
+    show_filenames=False,
+    color=False,
+    ignore_errors=False,
+):
+    if all_history:
+        files = chain(
+            glob.iglob(os.path.expanduser("~/.zsh_history")),
+            glob.iglob(os.path.expanduser("~/.bash_history")),
+            glob.iglob(os.path.expanduser(f"~/.history/**"), recursive=True),
+        )
+    elif pname:
+        files = glob.iglob(os.path.expanduser(f"~/.history/project/{pname}"), recursive=True)
     elif iterm_profile:
-        D='{}/.history/profiles/{}'.format(home, iterm_profile)
+        files = glob.iglob(
+            os.path.expanduser(f"~/.history/profiles/{iterm_profile}"), recursive=True
+        )
     else:
-        D='{}/.history'.format(home)
+        files = chain(
+            glob.iglob(os.path.expanduser("~/.zsh_history")),
+            glob.iglob(os.path.expanduser("~/.bash_history")),
+            glob.iglob(os.path.expanduser(f"~/.history/**"), recursive=True),
+        )
 
-    cmds = defaultdict(list)
+    fcmds = {}
     count = 0
-    for filename in glob.iglob('{}/*.history'.format(D), recursive=True):
-        prev = None
-        petime = None
-        for line in open(filename):
-            cmd, etime, rtime, continuation = getLine(line, prev)
-            if continuation:
-                cmds[petime][-1] = (cmds[petime][-1][0]+cmd, cmds[petime][-1][1])
-                continue
-            cmds[etime].append((cmd, rtime))
-            prev = line
-            petime = etime
-            count+=1
+    for filename in files:
+        if os.path.isdir(filename):
+            continue
+        cmd_continuation = False
+        cmds = defaultdict(list)
+        fcmds[filename] = cmds
+        cur_cmd = None
+        try:
+            for line in open(filename, encoding="utf-8", errors="replace"):
+                if not line.strip():
+                    if cur_cmd: ## Some commands end with a newline after a previous line that ends with \
+                        cur_cmd.cmd+=line
+                        cmds[cur_cmd.etime].append(cur_cmd)
+                        cur_cmd = None
+                        cmd_continuation = False
+                    continue
+                # Need a new command
+                if cmd_continuation:
+                    cur_cmd.cmd += line
+                else:
+                    if (line.startswith(": ") or line.startswith("#")): 
+                        if cur_cmd:
+                            cmds[cur_cmd.etime].append(cur_cmd)
+                        cur_cmd = Command()
+                        cmd_continuation = False
+                    if line.startswith(": "):  # zsh history
+                        s = line.strip().split(":", maxsplit=2)
+                        etime = datetime.fromtimestamp(int(s[1].strip()))
+                        rtime, cmd = s[2].split(";", maxsplit=1)
+                        cur_cmd.rtime = rtime
+                        cur_cmd.cmd = cmd
+                        cur_cmd.etime = etime
+                        cur_cmd.type = HistoryType.zsh
+                    elif line.startswith("#"):
+                        etime = datetime.fromtimestamp(int(line[1:]))
+                        cur_cmd.cmd = ""
+                        cur_cmd.etime = etime
+                        cur_cmd.type = HistoryType.bash
+                cmd_continuation = line.rstrip().endswith("\\")
 
+            if cur_cmd:
+                cmds[cur_cmd.etime].append(cur_cmd)
+        except Exception as e:
+            print(f"Parse Error '{filename}': \n{str(e)}", file=sys.stderr)
+            raise
+        if show_filenames:
+            if color:
+                print(f"{bcolors.GREEN}{filename}{bcolors.ENDC}")
+            else:
+                print(filename)
 
-    # example hist
-    #    1  2018-11-21 15:19:43  history
-    i = 1
-    l = max(5,len(str(count)))
-    fstr = "{:%d}  {}\t{}"%l
-    for kt in sorted(cmds.keys()):
-        cmdpair_list = cmds[kt]
-        for cmd, rtime in cmdpair_list:
-            print(fstr.format(i,kt, cmd.rstrip()))
-            i+=1
+        # example hist
+        #    1  2018-11-21 15:19:43  history
+        i = 1
+        l = max(5, len(str(count)))
+        fstr = "{:%d}  {}\t{}" % l
+        for kt in sorted(cmds.keys()):
+            cmdlist = cmds[kt]
+            for cmd in cmdlist:
+                print(fstr.format(i, kt, cmd.cmd.rstrip()))
+                i += 1
+
 
 if __name__ == "__main__":
-    home = os.environ.get('HOME')
-    pname = os.environ.get('PROJECT_NAME')
-    profile = os.environ.get('ITERM_PROFILE')
-    if len(sys.argv) == 3:
-        pname = sys.argv[1]
-        profile = sys.argv[2]
+    ## Only color if we are going to terminal
+    use_color = True if sys.stdout.isatty() else False
 
-    print_history(pname, profile)
+    parser = argparse.ArgumentParser(description="print shell history")
+    parser.add_argument("--all-history", action="store_true", help="show all history")
+    parser.add_argument("--show-filenames", action="store_true", help="show filenames")
+    parser.add_argument("--project-name", help="specify a project name")
+    parser.add_argument("--iterm-profile", help="specify the profile")
+    parser.add_argument(
+        "--ignore-errors", action="store_true", help="ignore certain errors while printing history"
+    )
+    parser.add_argument(
+        "--force-color", action="store_true", help="force color even in piped output"
+    )
+    parser.add_argument("--fc", action="store_true", help="force color even in piped output")
+
+    args = parser.parse_args()
+
+    print_history(
+        args.project_name,
+        args.iterm_profile,
+        all_history=args.all_history,
+        show_filenames=args.show_filenames,
+        color=args.force_color,
+        ignore_errors=args.ignore_errors,
+    )
--- piterm.profile
+++ piterm.profile
@@ -1,18 +1,64 @@
 #expand path
 PATH="$HOME/.piterm/bin:$PATH"
-source "$HOME/.piterm/advanced_history.profile"
+
+export P_ITERM_HOME="${HOME}/.piterm"
+export P_ITERM_TEMPLATES="${P_ITERM_HOME}/templates"
+export P_ITERM_PROJECTS_DIR="${P_ITERM_HOME}/projects"
+export P_ITERM_WORKSPACE_DIR="${HOME}/workspace"
+
+source "${P_ITERM_HOME}/advanced_history.profile"
 
 alias rproj=restore_project
 alias lproj=list_projects
 alias nproj=new_project
 
-
 ### tab completion for projects
-_compprojs (){
-	cur=${COMP_WORDS[COMP_CWORD]};
-	COMPREPLY=($(compgen -W "$(list_projects) " -- $cur))
+autoload -U +X compinit && compinit
+autoload -U +X bashcompinit && bashcompinit
+
+_compprojs() {
+	cd $P_ITERM_PROJECTS_DIR
+    cur=${COMP_WORDS[COMP_CWORD]}
+	ar=( $(compgen -f $P_ITERM_PROJECTS_DIR/$cur ) )
+	COMPREPLY=()
+	for c in ${ar[@]} ; do
+		if [[ -d $c ]] ; then 
+			COMPREPLY+=("$c/")
+		else
+			COMPREPLY+=("$c")
+		fi
+	done
+	COMPREPLY=("${COMPREPLY[@]%.*}")
+    return 0
 }
 
-complete -F _compprojs restore_project
+complete -F _compprojs -o nospace restore_project
 complete -F _compprojs new_project
 
+fix_project(){
+	local proj=$1
+	# SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+	# echo $SCRIPT_DIR "-" ${BASH_SOURCE[0]}
+	# while true; do
+	# 	echo -n 'Attempt to fix this project script? [Yn]:'
+	# 	read yn
+	# 	case $yn in
+			# [Yy]* ) 
+				## Find path locations and check to make sure there aren't multiple projects
+				local script_loc=$(echo $funcstack | tr -s ' ' | cut -d" " -f2-)
+				local proj_loc=$(find ~/workspace -type d -name $proj)
+				local c=$(echo $proj_loc | grep -c $proj)
+				echo "is $script_loc - $c - $proj_loc"
+				if [[ $c -eq 1 ]] ; then 
+					local n_proj=$(printf '%s\n' "$script_loc" | sed -e 's/[\/&]/\\&/g')
+					echo "n = $n_proj"
+					cat $script_loc | sed -e "s/^D=.*/D=$n_proj/"
+				else 
+					echo "not 1"
+				fi
+				echo "done"
+	# 			break;;
+	# 		* ) break;;
+	# 	esac
+	# done
+}
